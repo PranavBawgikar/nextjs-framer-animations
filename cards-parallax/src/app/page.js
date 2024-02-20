c00a0a7467082900ecea8d930f6ac61f1706837d
Pranav Bawgikar <102728016+PranavBawgikar@users.noreply.github.com>
2024-02-20 16:47:38 +0530

    Stacked Cards Parallax

--- cards-parallax/src/app/page.js
+++ cards-parallax/src/app/page.js
@@ -1,9 +1,37 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import { projects } from "@/data";
+import Card from '../components/Card'
+import { useScroll } from "framer-motion";
+import { useRef, useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 
 export default function Home() {
+  const container = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end end']
+  })
+
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  })
+  
   return (
-    <main className={styles.main}>
+    <main ref={container} className={styles.main}>
+      {
+        projects.map( (project, i) => {
+          const targetScale = 1 - ( (projects.length - i) * 0.05)
+          return <Card key={`p_${i}`} i={i} {...project} progress={scrollYProgress} range={[i * .25, 1]} targetScale={targetScale}/>
+        })
+      }
     </main>
   );
 }
