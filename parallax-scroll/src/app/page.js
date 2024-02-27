77a36e029a2287377e0804b180b45c082a0e84e8
Pranav Bawgikar <102728016+PranavBawgikar@users.noreply.github.com>
2024-02-27 13:53:30 +0530

    Update #2 Parallax Scroll

--- parallax-scroll/src/app/page.js
+++ parallax-scroll/src/app/page.js
@@ -1,8 +1,22 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import styles from "./page.module.scss";
 import Gsap from "@/components/Gsap/"
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 
 export default function Home() {
+
+  useEffect( () => {
+    const lenis = new Lenis()
+    
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+  }, [])
+
   return (
     <main className={styles.main}>
       <Gsap />
